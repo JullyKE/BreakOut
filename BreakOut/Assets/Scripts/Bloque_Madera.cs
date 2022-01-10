63b0461449be3aaa5213eceb5d4cab06df0ab073
Jully Kado Mercado Elias <jully_000@hotmail.com>
2022-01-10 11:15:08 -0600

    Modulo 10  Bloques
    Creación de bloques su script y material.

---
+++ BreakOut/Assets/Scripts/Bloque_Madera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloque_Madera : Bloque
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+	    resistencia = 3;
+    }
+    
+	public override void RebotarBola()
+	{
+		base.RebotarBola();
+	}
+}
