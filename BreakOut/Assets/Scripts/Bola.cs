bd776eb07fa9e5ac3dd7a6ec9c4d3db09735c52a
Jully Kado Mercado Elias <jully_000@hotmail.com>
2022-01-28 11:25:56 -0600

    Modulo 12
    Jugador y Bola

--- BreakOut/Assets/Scripts/Bola.cs
+++ BreakOut/Assets/Scripts/Bola.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Bola : MonoBehaviour
 {
+	public float ballSpeed = 10f;
+	bool isStart;
     // Start is called before the first frame update
     void Start()
     {
-        
+	    Vector3 posicionInicial = GameObject.FindGameObjectWithTag("Jugador").transform.position;
+	    posicionInicial.y += 3;
+	    transform.position = posicionInicial;
+	    transform.SetParent(GameObject.FindGameObjectWithTag("Jugador").transform);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+	    if (Input.GetKeyDown(KeyCode.Q) ||Input.GetButtonDown("Submit"))
+	    {
+	    	if (!isStart)
+	    	{
+	    		isStart = true;
+	    		transform.SetParent(null);
+	    		GetComponent<Rigidbody>().velocity = ballSpeed * Vector3.up;
+	    	}
+	    }
     }
 }
--- BreakOut/Assets/Scripts/Jugador.cs
+++ BreakOut/Assets/Scripts/Jugador.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Jugador : MonoBehaviour
 {
+	public int limiteX = 23;
+	public int velocidadPaddle = 5;
+	Vector3 mousePos2D;
+	Vector3 mousePos3D;
+	
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +17,50 @@ public class Jugador : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
+	{
+		//Mouse Es el que gusta mas
+	    /*mousePos2D = Input.mousePosition;
+	    mousePos2D.z = -Camera.main.transform.position.z;
+	    mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+	    */
+	    
+		//GetKey
+	    /*if (Input.GetKey(KeyCode.RightArrow))
+	    {
+	    	transform.Translate(Vector3.down * velocidadPaddle * Time.deltaTime);
+	    }
+	    if (Input.GetKey(KeyCode.LeftArrow))
+	    {
+	    	transform.Translate(Vector3.up * velocidadPaddle * Time.deltaTime);
+	    }*/
+	    
+		//GetAxis
+		/*float inputVertical = Input.GetAxis("Horizontal");
+		if (inputVertical > 0)
+		{
+			transform.Translate(Vector3.down * velocidadPaddle * Time.deltaTime);
+		}
+		if (inputVertical < 0)
+		{
+			transform.Translate(Vector3.up * velocidadPaddle * Time.deltaTime);
+		}*/
+		
+		//Control
+		transform.Translate(Input.GetAxis("Horizontal") * Vector3.down * velocidadPaddle * Time.deltaTime);
+		
+		
+	    
+	    Vector3 pos = transform.position;
+	    //pos.x = mousePos3D.x;
+	    if (pos.x < -limiteX)
+	    {
+	    	pos.x = -limiteX;
+	    }
+	    else if (pos.x > limiteX)
+	    {
+	    	pos.x = limiteX;
+	    }
+
+	    transform.position = pos;
     }
 }
